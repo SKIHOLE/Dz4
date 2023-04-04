2c2ee7c19c7c83a40bc08799e81e510ce84d749e
Iva <vanya12345654@gmail.com>
2023-04-04 11:13:36 +0300

    Добавьте файлы проекта.

---
+++ DZ4.3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ4._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player myPlayer = new Player();
+            myPlayer.Play();
+            myPlayer.Pause();
+            myPlayer.Stop();
+            myPlayer.Record();
+            myPlayer.Pause();
+            myPlayer.Stop();
+            Console.ReadLine();
+        }
+    }
+    interface IPlayable
+    {
+        void Play();
+        void Pause();
+        void Stop();
+    }
+    interface IRecodable
+    {
+        void Record();
+        void Pause();
+        void Stop();
+    }
+    class Player:IPlayable,IRecodable
+    {
+        public void Play() => Console.WriteLine("Start Playing");
+        public void Pause() => Console.WriteLine("Pause");
+        public void Stop() => Console.WriteLine("Stop");
+        public void Record() => Console.WriteLine("Start Recording");
+    }
+}
---
+++ Dz4.2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string DocumentType = Console.ReadLine();
+            AbstractHandler docHandler=null;
+            switch (DocumentType)
+            {
+                case "XMLHandler":
+                    docHandler = new XMLHandler();
+                    break;
+                case "DOCHandler":
+                    docHandler = new TXTHandler();
+                    break;
+                case "TXTHandler":
+                    docHandler = new DOCHandler();
+                    break;
+                default:
+                    Console.WriteLine("Invalid document type");
+                    break;
+            }
+            if(docHandler != null)
+            {
+                docHandler.Open();
+                docHandler.Create();
+                docHandler.Change();
+                docHandler.Save();
+            }
+        }
+    }
+    class AbstractHandler
+    {
+        public virtual void Open() => Console.WriteLine("XMLHandler.Open");
+        public virtual void Create() => Console.WriteLine("XMLHandler.Create");
+        public virtual void Change() => Console.WriteLine("XMLHandler.Change");
+        public virtual void Save() => Console.WriteLine("XMLHandler.Save");
+    }
+    class XMLHandler: AbstractHandler
+    {
+        public override void Open() => Console.WriteLine("XMLHandler.Open");
+        public override void Create() => Console.WriteLine("XMLHandler.Create");
+        public override void Change() => Console.WriteLine("XMLHandler.Change");
+        public override void Save() => Console.WriteLine("XMLHandler.Save");
+    }
+    class TXTHandler : AbstractHandler
+    {
+        public override void Open() => Console.WriteLine("TXTHandler.Open");
+        public override void Create() => Console.WriteLine("TXTHandler.Create");
+        public override void Change() => Console.WriteLine("TXTHandler.Change");
+        public override void Save() => Console.WriteLine("TXTHandler.Save");
+
+    }
+    class DOCHandler : AbstractHandler
+    {
+        public override void Open() => Console.WriteLine("DOCHandler.Open");
+        public override void Create() => Console.WriteLine("DOCHandler.Create");
+        public override void Change() => Console.WriteLine("DOCHandler.Change");
+        public override void Save() => Console.WriteLine("DOCHandler.Save");
+    }
+}
